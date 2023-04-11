c28dabf50c9a787a028f713daca658cc8a5c07e8
milkabaldi <justmainc8@gmail.com>
2023-04-11 18:06:32 +0300

    Changes

--- components/home/welcome/Welcome.jsx
+++ components/home/welcome/Welcome.jsx
@@ -21,8 +21,8 @@ const Welcome = ({ searchTerm, setSearchTerm, handleClick }) => {
   return (
     <View>
       <View style={styles.container}>
-        <Text style={styles.userName}>Hello Mihall</Text>
-        <Text style={styles.welcomeMessage}>Find your perfect job for </Text>
+        <Text style={styles.userName}>Hello Mihal</Text>
+        <Text style={styles.welcomeMessage}>Find your perfect job</Text>
       </View>
 
       <View style={styles.searchContainer}>
