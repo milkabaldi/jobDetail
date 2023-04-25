4149ec4a94a090e679fc8be9e6483f6ccbdde1a3
milkabaldi <justmainc8@gmail.com>
2023-04-25 20:40:06 +0300

    Card selecte Job

--- components/common/cards/popular/PopularJobCard.jsx
+++ components/common/cards/popular/PopularJobCard.jsx
@@ -10,6 +10,13 @@ const PopularJobCard = () => {
             <Image source={{url: item.employer_logo}} resizeMode='contain' style={styles.logoImage} />
         </TouchableOpacity>
         <Text style={style.companyName} numberOfLines={1}>{item.employer_name}</Text>
+
+
+      <View style={styles.infoContainer}>
+        <Text style={styles.jobName(selectedJob, item)}>
+          {item.job_title}
+        </Text>
+      </View>
     </TouchableOpacity>
   )
 }
